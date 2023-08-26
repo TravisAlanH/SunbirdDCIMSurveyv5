a3cb2f933b036539a21e77fd9857a54a8ab8c961
TravisAlanH <70112002+TravisAlanH@users.noreply.github.com>
2023-08-26 16:05:23 -0500

    Update Modal2.js
    Hold for refactor of DataBase

--- src/Components/Body/Modal2.js
+++ src/Components/Body/Modal2.js
@@ -2,17 +2,18 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import * as actions from "../../Slices/CounterSlice";
 
-// CHECKS : "Type", "Condition", "Damage", "Note", "GPS"
-
 export default function Modal() {
   const BASE_DATA = useSelector((state) => state.location.Location[0]);
-  const [currentLocationIndex, setCurrentLocationIndex] = React.useState(0);
-  const [currentRoomIndex, setCurrentRoomIndex] = React.useState(0);
-  const [currentAssetsIndex, setCurrentAssetsIndex] = React.useState(0);
-  const [currentRacksIndex, setCurrentRacksIndex] = React.useState(0);
+  const CURRENT = useSelector((state) => state.location.Current);
+  function textCleanUp(text) {
+    return text
+      .replace("dcTrack", "dcT")
+      .replace("Data Center", "DC")
+      .replace("Number", "Num")
+      .replace(/[^a-zA-Z0-9 ]/g, "")
+      .slice(0, 12);
+  }
 
-  let setIndex = [setCurrentLocationIndex, setCurrentRoomIndex, setCurrentAssetsIndex, setCurrentRacksIndex];
-  let useIndex = [currentLocationIndex, currentRoomIndex, currentAssetsIndex, currentRacksIndex];
   const dispatch = useDispatch();
   let payload = {
     index: 0,
@@ -35,7 +36,7 @@ export default function Modal() {
           //! Block sets up MODAL Clicks
           <div
             key={modalIndex}
-            className="w-[20rem] h-[7rem] rounded-lg py-2 px-4 bg-slate-500"
+            className="w-[20rem] h-[8.5rem] rounded-xl py-2 px-4 bg-slate-500"
             id="myBtn"
             onClick={() => {
               let modal = document.getElementById("modal" + modalIndex);
@@ -54,58 +55,138 @@ export default function Modal() {
             }}
           >
             {/* DATA IN BLOCK */}
-            <div>
-              {/* HEAD OF MODAL */}
-              <div className="flex flex-row justify-between items-center">
-                <h2 className="font-bold">{modalBlock.split(/(?=[A-Z])/)[0]}</h2>
-                <div className="flex flex-row items-center">
-                  <div>
-                    <select
-                      className="max-w-[10rem] min-w-[10rem] min-h-8 border-2 border-gray-300 rounded-md"
-                      onClick={(e) => e.stopPropagation()}
-                      onChange={(e) => {
-                        let set = setIndex[modalIndex];
-                        set(e.target.options.selectedIndex);
-                      }}
-                    >
-                      {BASE_DATA[modalBlock].map((item, index) => (
-                        <option key={index} value={index}>
-                          {BASE_DATA[modalBlock][index]["ID"] !== "" ? BASE_DATA[modalBlock][index]["ID"] : `New ${index + 1}`}
-                        </option>
-                      ))}
-                    </select>
-                  </div>
+            <div className="w-full h-full flex flex-col justify-between">
+              <div>
+                {/* HEAD OF MODAL */}
+                <div className="flex flex-row justify-between items-center">
+                  <h2 className="font-bold text-lg">{modalBlock.split(/(?=[A-Z])/)[0]}</h2>
+                  {/* <div className="flex flex-row items-center pb-2">
+                  <select
+                    id={"ModalHeader" + modalIndex}
+                    className="max-w-[10rem] min-w-[10rem] selectBox border-2 border-gray-300 rounded-md"
+                    onClick={(e) => e.stopPropagation()}
+                    onChange={(e) => {
+                      // let set = setIndex[modalIndex];
+                      // set(e.target.options.selectedIndex);
+
+                      // Testing Store index
+                      payload.key = modalBlock + "Index";
+                      payload.value = e.target.options.selectedIndex;
+                      dispatch(actions.updateCurrent(payload));
+
+                      // let updateIndexArray = indexArray;
+                      // updateIndexArray[modalIndex] = e.target.options.selectedIndex;
+                      // setIndexArray(updateIndexArray);
+                    }}
+                  >
+                    <option value={0}>Select</option>
+                    {BASE_DATA[modalBlock].map((item, index) => (
+                      <option key={index} value={index}>
+                        {BASE_DATA[modalBlock][index]["ID"] !== "" ? BASE_DATA[modalBlock][index]["ID"] : `New ${index + 1}`}
+                      </option>
+                    ))}
+                  </select>
                   <button
                     className="border-2 border-gray-300 bg-slate-300 rounded-md w-8 h-8"
                     onClick={(e) => {
                       e.stopPropagation();
                       payload.modalType = modalBlock;
                       dispatch(actions.addToArray(payload));
+                      payload.key = modalBlock + "Index";
+                      payload.value = BASE_DATA[modalBlock].length;
+                      dispatch(actions.updateCurrent(payload));
+                      setTimeout(() => {
+                        document.getElementById("ModalHeader" + modalIndex).lastElementChild.selected = true;
+                      }, 200);
+                    }}
+                  >
+                    +
+                  </button>
+                </div> */}
+                </div>
+                {/* MISSING DATA IN MODAL */}
+                <div>
+                  <div className="flex flex-row justify-start border-b-2">
+                    {BASE_DATA[modalBlock].length > 0 ? (
+                      <div>
+                        <h2 className="text-sm">Required Actions</h2>
+                      </div>
+                    ) : (
+                      <div>
+                        <h2 className="text-sm">Use the + to add {modalBlock.split(/(?=[A-Z])/)[0]}</h2>
+                      </div>
+                    )}
+                  </div>
+                  {BASE_DATA[modalBlock]
+                    // .filter((_, index) => index === indexArray[modalIndex])
+                    .filter((_, index) => index === CURRENT[modalBlock + "Index"])
+                    .map((item, index) => (
+                      <div key={index} className="flex flex-row gap-2">
+                        {Object.keys(BASE_DATA[modalBlock][index])
+                          .filter((item) => item.includes("*"))
+                          .filter((item) => BASE_DATA[modalBlock][CURRENT[modalBlock + "Index"]][item] === "")
+                          .slice(0, 4)
+                          .map((item, index2, array) => {
+                            if (index2 < 3) {
+                              return (
+                                <div key={index2}>
+                                  <div className="flex flex-row justify-start">
+                                    <label>{textCleanUp(item) + ","}</label>
+                                  </div>
+                                </div>
+                              );
+                            } else {
+                              return (
+                                <div key={index2}>
+                                  <div className="flex flex-row justify-start">
+                                    <label>{"..."}</label>
+                                  </div>
+                                </div>
+                              );
+                            }
+                          })}
+                      </div>
+                    ))}
+                </div>
+              </div>
+              {/* MODAL SELECT / ADD */}
+              <div>
+                <div className="flex flex-row items-end gap-2 justify-end mb-2">
+                  <select
+                    id={"ModalHeader" + modalIndex}
+                    className="max-w-[10rem] min-w-[10rem] selectBox border-2 border-gray-300 rounded-md"
+                    onClick={(e) => e.stopPropagation()}
+                    onChange={(e) => {
+                      payload.key = modalBlock + "Index";
+                      payload.value = e.target.options.selectedIndex;
+                      dispatch(actions.updateCurrent(payload));
+                    }}
+                  >
+                    <option value={0}>Select</option>
+                    {BASE_DATA[modalBlock].map((item, index) => (
+                      <option key={index} value={index}>
+                        {BASE_DATA[modalBlock][index]["ID"] !== "" ? BASE_DATA[modalBlock][index]["ID"] : `New ${index + 1}`}
+                      </option>
+                    ))}
+                  </select>
+                  <button
+                    className="border-2 border-gray-300 bg-slate-300 rounded-md w-8 h-8"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      payload.modalType = modalBlock;
+                      dispatch(actions.addToArray(payload));
+                      payload.key = modalBlock + "Index";
+                      payload.value = BASE_DATA[modalBlock].length;
+                      dispatch(actions.updateCurrent(payload));
+                      setTimeout(() => {
+                        document.getElementById("ModalHeader" + modalIndex).lastElementChild.selected = true;
+                      }, 200);
                     }}
                   >
                     +
                   </button>
                 </div>
               </div>
-
-              {/* MISSING DATA IN MODAL */}
-              {BASE_DATA[modalBlock]
-                .filter((_, index) => index === useIndex[modalIndex])
-                .map((item, index) => (
-                  <div key={index}>
-                    {Object.keys(BASE_DATA[modalBlock][index])
-                      .filter((item) => item.includes("*"))
-                      .filter((item) => BASE_DATA[modalBlock][useIndex[modalIndex]][item] === "")
-                      .map((item, index2) => (
-                        <div key={index2}>
-                          <div className="flex flex-row justify-start">
-                            <label>{item}</label>
-                            <label>{BASE_DATA[modalBlock][index]["Name*"]}</label>
-                          </div>
-                        </div>
-                      ))}
-                  </div>
-                ))}
             </div>
             {/* MODAL CONTENT */}
             <div className="modal" id={"modal" + modalIndex}>
@@ -121,15 +202,17 @@ export default function Modal() {
                   <div className="flex flex-row justify-between items-center w-full">
                     <button
                       onClick={() => {
-                        let set = setIndex[modalIndex];
-                        set(setNewCurrentShowLength(false, BASE_DATA[modalBlock].length, useIndex[modalIndex]));
+                        payload.key = modalBlock + "Index";
+                        payload.value = setNewCurrentShowLength(false, BASE_DATA[modalBlock].length, CURRENT[modalBlock + "Index"]);
+                        dispatch(actions.updateCurrent(payload));
+                        console.log(CURRENT[modalBlock + "Index"]);
                       }}
                     >
                       {"<"}
                     </button>
                     <div>
                       {BASE_DATA[modalBlock]
-                        .filter((_, index) => index === useIndex[modalIndex])
+                        .filter((_, index) => index === CURRENT[modalBlock + "Index"])
                         .map((item, index) => (
                           <div key={index}>
                             {Object.keys(BASE_DATA[modalBlock][index]).map((item2, index2) => (
@@ -141,13 +224,13 @@ export default function Modal() {
                                   <input
                                     className="w-full border-2 border-gray-300 rounded-md"
                                     type="text"
-                                    value={BASE_DATA[modalBlock][useIndex[modalIndex]][item2]}
+                                    value={BASE_DATA[modalBlock][CURRENT[modalBlock + "Index"]][item2]}
                                     onChange={(e) => {
                                       payload.index = index;
                                       payload.key = item2;
                                       payload.value = e.target.value;
                                       payload.modalType = modalBlock;
-                                      payload.arrayIndex = useIndex[modalIndex];
+                                      payload.arrayIndex = CURRENT[modalBlock + "Index"];
                                       dispatch(actions.updateKeyValueIn(payload));
                                     }}
                                   />
@@ -159,8 +242,10 @@ export default function Modal() {
                     </div>
                     <button
                       onClick={() => {
-                        let set = setIndex[modalIndex];
-                        set(setNewCurrentShowLength(true, BASE_DATA[modalBlock].length, useIndex[modalIndex]));
+                        payload.key = modalBlock + "Index";
+                        payload.value = setNewCurrentShowLength(true, BASE_DATA[modalBlock].length, CURRENT[modalBlock + "Index"]);
+                        dispatch(actions.updateCurrent(payload));
+                        console.log(CURRENT[modalBlock + "Index"]);
                       }}
                     >
                       {">"}
