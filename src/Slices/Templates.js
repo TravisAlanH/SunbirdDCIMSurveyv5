e45fd8668e51cce7734872caefc9a1a076472065
TravisAlanH <70112002+TravisAlanH@users.noreply.github.com>
2023-08-26 16:28:54 -0500

    Templates Comments
    Commented on Templates the Chagnes

--- src/Slices/Templates.js
+++ src/Slices/Templates.js
@@ -79,5 +79,53 @@ const State = {
   },
 };
 
+// Fix Location Refactor to be Object not Array
+
+// turn Modal.js into component, passing in State2.Location[//Section from something//].Sections Below (Not the LocationData)
+
+const State2 = {
+  Location: [
+    {
+      LocationData: {
+        // will be larger Modal
+        //hold standard Data
+      },
+      RoomDataArray: [], // change these to objects not arrays, that hold Array Data
+      AssetsArray: [],
+      RacksArray: [],
+      Electrical: {
+        Panels: [],
+        Receptacles: [],
+        UPS: [],
+        RackATS: [],
+        RackPDU: [],
+        GroundBus: [],
+        Other: [],
+      },
+      Mechanical: {
+        "@COLOR": "", // Set color for each section
+        AirConditioning: [],
+        Humidifier: [],
+      },
+      FireDetectionProtection: {
+        Detection: [],
+        Protection: [],
+        Rated: [],
+      },
+      FiberTrunkCabling: {
+        LIU: [],
+      },
+    },
+  ],
+  Current: {
+    "dcTrack Location Name*": "",
+    "Room Number": "",
+    "LocationIndex": 0,
+    "RoomArrayIndex": 0,
+    "AssetsArrayIndex": 0,
+    "RacksArrayIndex": 0,
+  },
+};
+
 export { State };
 export default Templates;
