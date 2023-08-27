1596c9fc0a31c826ba0ad0a326f2bbfe17750b34
TravisAlanH <70112002+TravisAlanH@users.noreply.github.com>
2023-08-27 11:17:38 -0500

    Responsive Update

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -6,7 +6,7 @@ export default function Body() {
   const State = useSelector((state) => state.location.Location[0]);
   return (
     // <div className="w-full flex flex-col items-center">
-    <div className="grid grid-cols-2 gap-3">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
       {Object.keys(State).map((DataItem, DataIndex) => (
         <Modal data={State[DataItem]} ObjKey={DataItem} key={DataIndex} />
       ))}
