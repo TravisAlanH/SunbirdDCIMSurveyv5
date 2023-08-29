f77eb7478d23733be333838af3ce39f5951497f1
TravisAlanH <70112002+TravisAlanH@users.noreply.github.com>
2023-08-28 19:50:48 -0500

    Rack Search Added

--- src/Components/Inputs/SearchInput.js
+++ src/Components/Inputs/SearchInput.js
@@ -1,99 +1,119 @@
 import React from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import * as actions from "../../Slices/CounterSlice";
-// import FindMatch from "../../Reuse/FindMatch";
+import { useSelector, useDispatch } from "react-redux";
+import * as actions from "../../Slices/CounterSlice";
+import FindMatch from "../../Reuse/FindMatch";
 // import Devices from "../../Data/4Device";
-// import Racks from "../../Data/3Cabinet";
+import Racks from "../../Data/3Cabinet";
 
-export default function SearchInput({ payload, modalBlock, ItemKey, index, data }) {
-  // const [makeArray, setMakeArray] = React.useState([]);
-  // const [inputText, setInputText] = React.useState("");
-  // const CURRENT = useSelector((state) => state.location.Current);
-  // const dispatch = useDispatch();
+export default function SearchInput({ modalBlock, ItemKey, index, data }) {
+  const [modelArray, setModelArray] = React.useState([]);
+  const [inputText, setInputText] = React.useState("");
+  const CURRENT = useSelector((state) => state.location.Current);
+  const dispatch = useDispatch();
   // const BASE_DATA = data;
 
-  // let SearchData = {
-  //   AssetsArray: {
-  //     Data: Devices,
-  //     Search: "Model Name *",
-  //   },
-  //   RacksArray: {
-  //     Data: Racks,
-  //     Search: "Model Name *",
-  //   },
-  // };
+  // {
+  //   MAKE: "Alpha Technologies",
+  //   MODEL: "031-322-20 DC Battery Stand",
+  //   CLASS: "Cabinet",
+  //   RU: "48",
+  // },
 
-  // let SearchArray = SearchData[modalBlock].Data;
-  // let SearchTerm = SearchData[modalBlock].Search;
+  console.log(modalBlock);
 
-  return <></>;
-  // return (
-  //   <div className="dropdown" onClick={(e) => e.stopPropagation()}>
-  //     <input
-  //       type="text"
-  //       // id={"Model" + MatchedIndex}
-  //       className="dropbtn w-[7.5rem]"
-  //       value={inputText}
-  //       onChange={(e) => {
-  //         e.stopPropagation();
-  //         setInputText(e.target.value);
-  //         setMakeArray(FindMatch(e.target.value, SearchArray, SearchTerm));
-  //         setTimeout(() => {
-  //           payload.key = { ItemKey };
-  //           payload.value = e.target.value;
-  //           dispatch(actions.updateKeyValueIn(payload));
-  //         }, 1000);
-  //       }}
-  //     />
-  //     <div className="dropdown-content">
-  //       {makeArray.map((SearchedItem, SearchedIndex) => {
-  //         return (
-  //           <button
-  //             key={SearchedIndex}
-  //             onClick={(e) => {
-  //               e.stopPropagation();
-  //               e.preventDefault();
-  //               setInputText(SearchArray[SearchedItem]["Model Name *"]);
-  //               if (modalBlock === "RacksArray") {
-  //                 setTimeout(() => {
-  //                   payload.key = "Model";
-  //                   payload.value = Racks[SearchedItem]["Model Name *"];
+  let KeySearching = "MODEL";
+  let KeySearching2 = "MAKE";
+  let KeySearching3 = "RU";
+  let KeySearching4 = "CLASS";
 
-  //               }
-  //               setTimeout(() => {
-  //                 payload.key = "Model";
-  //                 payload.value = Devices[SearchedItem]["Model Name *"];
-  //                 dispatch(actions.updateKeyValueIn(payload));
-  //                 console.log(payload.value);
-  //                 setTimeout(() => {
-  //                   payload.key = "Make";
-  //                   payload.value = Devices[SearchedItem]["Make *"];
-  //                   dispatch(actions.updateKeyValueIn(payload));
-  //                 }, 100);
-  //               }, 111);
-  //             }}
-  //           >
-  //             {SearchArray[SearchedItem][SearchTerm]}
-  //           </button>
-  //         );
-  //       })}
-  //     </div>
-  //   </div>
+  let KeyChanging = "Model";
+  let KeyChanging2 = "Make";
+  let KeyChanging3 = "Height in UP";
+  let KeyChanging4 = "Type";
+  let KeyChanging5 = "RU Available";
 
-  // <input
-  //   className="block w-full  px-1 ModInput"
-  //   placeholder="test"
-  //   type="text"
-  //   value={BASE_DATA[modalBlock][CURRENT[modalBlock + "Index"]][ItemKey]}
-  //   onChange={(e) => {
-  //     payload.index = index;
-  //     payload.key = ItemKey;
-  //     payload.value = e.target.value;
-  //     payload.modalType = modalBlock;
-  //     payload.state = BASE_DATA[modalBlock];
-  //     payload.arrayIndex = CURRENT[modalBlock + "Index"];
-  //     dispatch(actions.updateKeyValueIn(payload));
-  //   }}
-  // />
-  // );
+  let payload = {
+    index: 0,
+    ObjKey: "Racks",
+    modalType: "RacksArray",
+    arrayIndex: CURRENT.RacksArrayIndex,
+  };
+
+  return (
+    <div className="dropdown" onClick={(e) => e.stopPropagation()}>
+      <input
+        type="text"
+        // id={"Model" + MatchedIndex}
+        className="dropbtn w-[7.5rem]"
+        value={inputText}
+        onChange={(e) => {
+          e.stopPropagation();
+          setInputText(e.target.value);
+          setModelArray(FindMatch(e.target.value, Racks, KeySearching));
+          setTimeout(() => {
+            payload.key = KeyChanging;
+            payload.value = e.target.value;
+            dispatch(actions.updateKeyValueIn(payload));
+          }, 1000);
+        }}
+      />
+      <div className="dropdown-content">
+        {modelArray.map((SearchedItem, SearchedIndex) => {
+          return (
+            <button
+              key={SearchedIndex}
+              onClick={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                setInputText(Racks[SearchedItem][KeySearching]);
+                console.log(Racks[SearchedItem][KeySearching]);
+                setTimeout(() => {
+                  payload.key = KeyChanging;
+                  payload.value = Racks[SearchedItem][KeySearching];
+                  dispatch(actions.updateKeyValueIn(payload));
+                  setTimeout(() => {
+                    payload.key = KeyChanging2;
+                    payload.value = Racks[SearchedItem][KeySearching2];
+                    dispatch(actions.updateKeyValueIn(payload));
+                    setTimeout(() => {
+                      payload.key = KeyChanging3;
+                      payload.value = Racks[SearchedItem][KeySearching3];
+                      dispatch(actions.updateKeyValueIn(payload));
+                      setTimeout(() => {
+                        payload.key = KeyChanging4;
+                        payload.value = Racks[SearchedItem][KeySearching4];
+                        dispatch(actions.updateKeyValueIn(payload));
+                        setTimeout(() => {
+                          payload.key = KeyChanging5;
+                          payload.value = Racks[SearchedItem][KeySearching3];
+                          dispatch(actions.updateKeyValueIn(payload));
+                        }, 100);
+                      }, 100);
+                    }, 100);
+                  }, 100);
+                }, 100);
+                // setTimeout(() => {
+                //   payload.key = "Model";
+                //   payload.value = Racks[SearchedItem]["Model Name *"];
+                //   setTimeout(() => {
+                //     payload.key = "Model";
+                //     payload.value = Devices[SearchedItem]["Model Name *"];
+                //     dispatch(actions.updateKeyValueIn(payload));
+                //     console.log(payload.value);
+                //     setTimeout(() => {
+                //       payload.key = "Make";
+                //       payload.value = Devices[SearchedItem]["Make *"];
+                //       dispatch(actions.updateKeyValueIn(payload));
+                //     }, 100);
+                //   }, 111);
+                // }, 111);
+              }}
+            >
+              {Racks[SearchedItem][KeySearching]}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
