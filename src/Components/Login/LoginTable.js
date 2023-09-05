bd5ee0e5f066230c61fcf853a0bae3626fdf5a5b
TravisAlanH <70112002+TravisAlanH@users.noreply.github.com>
2023-09-05 09:57:21 -0500

    Update LoginTable.js

--- src/Components/Login/LoginTable.js
+++ src/Components/Login/LoginTable.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Logo from "../../Img/sunbird-logo-white.png";
-import axios from "axios";
+// import axios from "axios";
 import * as actions from "../../Slices/CounterSlice";
 import { useDispatch } from "react-redux";
+import axios from "axios";
 
-export default function LoginTable({ setAssetData, setRackData }) {
+export default function LoginTable() {
   const dispatch = useDispatch();
   const baseLogin = process.env.REACT_APP_LOGIN;
   const basePass = process.env.REACT_APP_PASS;
@@ -13,18 +14,33 @@ export default function LoginTable({ setAssetData, setRackData }) {
     set: 1,
   };
 
-  let fullURL = process.env.REACT_APP_BASEURL;
-  let URLRack = process.env.REACT_APP_RACKURL;
-  let URLAssets = process.env.REACT_APP_DEVICEURL;
+  //   function Login(e) {
+  //     e.preventDefault();
+  //     // e.preventDefault();
+  //     let user = document.getElementById("user").value;
+  //     let pass = document.getElementById("pass").value;
+  //     // let domain = document.getElementById("domain").value;
 
-  async function getData() {
-    await axios.get(fullURL + URLAssets).then((res) => {
-      setAssetData(res.data);
-    });
-    await axios.get(fullURL + URLRack).then((res) => {
-      setRackData(res.data);
-    });
-  }
+  //     if (user === baseLogin && pass === basePass) {
+  //       dispatch(actions.loginInOut(payload));
+  //     }
+
+  // axios
+  //   .get(`/api?type=makes&user=${user}&pass=${pass}&domain=${domain}`)
+  //   .then((res) => {
+  //     console.log(res);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+
+  // fetch(`/api?type=makes&user=${user}&pass=${pass}&domain=${domain}`)
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data.message));
+  // console.log(data);
+  //   }
+
+  let FULL_URL = process.env.REACT_APP_BASEURL;
 
   return (
     <div className="h-[31rem] w-[17.5rem] flex flex-col pt-[1rem] justify-center items-center relative bg-[rgba(16,16,16,0.68);] rounded-md">
@@ -51,7 +67,20 @@ export default function LoginTable({ setAssetData, setRackData }) {
               // let domain = document.getElementById("domain").value;
               if (user === baseLogin && pass === basePass) {
                 dispatch(actions.loginInOut(payload));
-                getData();
+                setTimeout(() => {
+                  let AddURL = process.env.REACT_APP_RACKURL;
+                  axios.get(FULL_URL + AddURL).then((res) => {
+                    dispatch(actions.getRackData(res.data));
+                    // Data = res.data;
+                  });
+                  setTimeout(() => {
+                    let AddURL = process.env.REACT_APP_DEVICEURL;
+                    axios.get(FULL_URL + AddURL).then((res) => {
+                      dispatch(actions.getAssetData(res.data));
+                      // Data = res.data;
+                    });
+                  }, 1000);
+                }, 1000);
               }
             }}
           >
